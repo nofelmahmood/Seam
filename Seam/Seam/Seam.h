b00a20f31671a0f2aa1496edc1fbf5e8e3cca7e6
nofelmahmood <nofelmehmood@gmail.com>
2015-08-12 12:22:16 +0500

    Commit

---
+++ Seam/Seam/Seam.h
@@ -0,0 +1,21 @@
+//
+//  Seam.h
+//  Seam
+//
+//  Created by Nofel Mahmood on 12/08/2015.
+//  Copyright © 2015 CloudKitSpace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Seam.
+FOUNDATION_EXPORT double SeamVersionNumber;
+
+//! Project version string for Seam.
+FOUNDATION_EXPORT const unsigned char SeamVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Seam/PublicHeader.h>
+
+
+
+

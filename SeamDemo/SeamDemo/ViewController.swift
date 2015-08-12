90244755b0453d0fbe847b1ae1a8886c5a67df4d
nofelmahmood <nofelmehmood@gmail.com>
2015-08-12 17:41:54 +0500

    Commit

---
+++ SeamDemo/SeamDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeamDemo
+//
+//  Created by Nofel Mahmood on 12/08/2015.
+//  Copyright © 2015 CloudKitSpace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
